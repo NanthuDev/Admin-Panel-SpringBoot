11ca223ac13ba2a6e84368524d4121a4f9608ec6
NanthuSSI <35912811+NanthuDev@users.noreply.github.com>
2024-07-09 00:49:23 +0530

    Create AdminPanelApplication.java

---
+++ src/main/java/com/example/admin_panel/AdminPanelApplication.java
@@ -0,0 +1,15 @@
+package com.example.admin_panel;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPanelApplication.class, args);
+	}
+
+}
